d28efe0c4bc06662a9c1c0b369e96e7be54a3184
rolandm99 <manfulmweze99@gmail.com>
2022-04-26 10:05:27 +0100

    Fix linters errors

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,45 +1,49 @@
-require "test_helper"
+require 'test_helper'
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @item = items(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get items_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_item_url
     assert_response :success
   end
 
-  test "should create item" do
-    assert_difference("Item.count") do
-      post items_url, params: { item: { description: @item.description, image: @item.image, name: @item.name, price: @item.price, title: @item.title } }
+  test 'should create item' do
+    assert_difference('Item.count') do
+      post items_url,
+           params: { item: { description: @item.description, image: @item.image, name: @item.name, price: @item.price,
+                             title: @item.title } }
     end
 
     assert_redirected_to item_url(Item.last)
   end
 
-  test "should show item" do
+  test 'should show item' do
     get item_url(@item)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_item_url(@item)
     assert_response :success
   end
 
-  test "should update item" do
-    patch item_url(@item), params: { item: { description: @item.description, image: @item.image, name: @item.name, price: @item.price, title: @item.title } }
+  test 'should update item' do
+    patch item_url(@item),
+          params: { item: { description: @item.description, image: @item.image, name: @item.name, price: @item.price,
+                            title: @item.title } }
     assert_redirected_to item_url(@item)
   end
 
-  test "should destroy item" do
-    assert_difference("Item.count", -1) do
+  test 'should destroy item' do
+    assert_difference('Item.count', -1) do
       delete item_url(@item)
     end
 
--- test/controllers/reservations_controller_test.rb
+++ test/controllers/reservations_controller_test.rb
@@ -1,45 +1,47 @@
-require "test_helper"
+require 'test_helper'
 
 class ReservationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @reservation = reservations(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get reservations_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_reservation_url
     assert_response :success
   end
 
-  test "should create reservation" do
-    assert_difference("Reservation.count") do
-      post reservations_url, params: { reservation: { end_time: @reservation.end_time, start_time: @reservation.start_time } }
+  test 'should create reservation' do
+    assert_difference('Reservation.count') do
+      post reservations_url,
+           params: { reservation: { end_time: @reservation.end_time, start_time: @reservation.start_time } }
     end
 
     assert_redirected_to reservation_url(Reservation.last)
   end
 
-  test "should show reservation" do
+  test 'should show reservation' do
     get reservation_url(@reservation)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_reservation_url(@reservation)
     assert_response :success
   end
 
-  test "should update reservation" do
-    patch reservation_url(@reservation), params: { reservation: { end_time: @reservation.end_time, start_time: @reservation.start_time } }
+  test 'should update reservation' do
+    patch reservation_url(@reservation),
+          params: { reservation: { end_time: @reservation.end_time, start_time: @reservation.start_time } }
     assert_redirected_to reservation_url(@reservation)
   end
 
-  test "should destroy reservation" do
-    assert_difference("Reservation.count", -1) do
+  test 'should destroy reservation' do
+    assert_difference('Reservation.count', -1) do
       delete reservation_url(@reservation)
     end
 
