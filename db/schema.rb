7b9be360ace33e161d14915267d7dd7aa96687c7
aakbarkhan <aakbarkhan@gmail.com>
2022-04-27 14:08:49 +0530

    Created schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_26_085726) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_27_083539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "title"
-    t.integer "price"
     t.string "description"
     t.string "image"
+    t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
