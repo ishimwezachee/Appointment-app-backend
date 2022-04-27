b6e4886b4212923de2578fc121c55b4b5100c541
aakbarkhan <aakbarkhan@gmail.com>
2022-04-27 14:36:15 +0530

    Add routes for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :items
   resources :reservations
-  resources :users
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "users#index"
 end
