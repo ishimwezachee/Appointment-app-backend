aa9af463f54c07754e9c190729fdc5bd8c87fdf5
aakbarkhan <aakbarkhan@gmail.com>
2022-04-27 14:39:01 +0530

    Add the schema

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+# user1 = User.create(name: 'Gints')
+
+# item1 = Item.create(name: 'Gints', title: 'Speciality', description: 'Really a good dr.', image: 'https://avatars.githubusercontent.com/u/43877689?v=4', price: 100)
