110c6fc13bdf2f9c8696f3aa42e44801517eeda4
rolandm99 <manfulmweze99@gmail.com>
2022-04-28 09:59:38 +0100

    Fix error

--- app/models/Rating.rb
+++ app/models/Rating.rb
@@ -2,6 +2,7 @@ class Rating < ApplicationRecord
   belongs_to :item
   belongs_to :user
 
+  # method to update the rating
   def update_rating
     item.increment!(:likes_counter)
   end
--- app/models/Review.rb
+++ app/models/Review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :item
   belongs_to :user
 
+  # method to update the reviews counter
   def update_reviews_counter
     item.update(reviews_counter: item.reviews.count)
   end
