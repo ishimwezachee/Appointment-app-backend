f5f87dd7c4f75d17e1659e0e3ca0ef5b5ae6b704
aakbarkhan <aakbarkhan@gmail.com>
2022-05-05 16:28:39 +0530

    Made cahnges to the user Doc

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -2,40 +2,99 @@ require 'swagger_helper'
 
 RSpec.describe '/users', type: :request do
   path '/users' do
-    post 'Creates a user' do
+    get('list users') do
       tags 'Users'
-      consumes 'application/json'
-      parameter name: :user, in: :body, schema: {
-        type: :object,
-        properties: {
-          name: { type: :string },
-          email: { type: :string },
-          password: { type: :string },
-          password_confirmation: { type: :string }
-        },
-        required: %w[name email password password_confirmation]
-      }
-
-      response '201', 'user created' do
-        let(:user) { { name: 'foo', email: 'bar', password: '123456', password_confirmation: '123456' } }
-        run_test!
-      end
-
-      response '422', 'invalid request' do
-        let(:user) { { name: 'foo', email: 'bar' } }
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
         run_test!
       end
     end
   end
 
-  path '/users/{id}' do
-    get 'Retrieves a User' do
-      tags 'Users'
-      produces 'application/json', 'application/xml'
-      parameter name: :id, in: :path, type: :string
+  post('create an User') do
+    tags 'Users'
+    parameter name: :user, in: :body, required: true, schema: {
+        type: :object,
+        properties: {
+          user: {
+            type: :object,
+            properties: {
+              name: { type: :string },
+              email: { type: :string },
+              password: { type: :integer },
+              password_confirmation: { type: :string }
+            },
+            required: %w[name email password password_confirmation ]
+          }
+        }
+    }
+    response(201, 'successful') do
+        after do |example|
+            example.metadata[:response][:content] = {
+                'application/json' => {
+                    example: JSON.parse(response.body, symbolize_names: true)
+                }
+            }
+            let(:user) do 
+                { name: 'Gints', email: 'foo', password: '123456', password_confirmation: '123456' }
+            end
+            run_test!
+        end
+    end
+  end
+end
 
-      response '200', 'user found' do
-        let(:id) { User.create(name: 'foo', email: 'bar', password: '123456', password_confirmation: '123456').id }
+RSpec.describe '/users', type: :request do
+    path '/users/{id}' do
+        get 'Retrieves a User' do
+        tags 'Users'
+        produces 'application/json', 'application/xml'
+        parameter name: :id, in: :path, type: :string
+
+            response '200', 'user found' do
+            let(:id) { User.create( name: 'Gints', email: 'foo', password: '123456', password_confirmation: '123456').id }
+            run_test!
+            end
+        end
+    end
+
+end
+
+
+RSpec.describe 'users post', type: :request do
+  path '/users' do
+    post('create user') do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, required: true, schema: {
+        type: :object,
+        properties: {
+          user: {
+            type: :object,
+            properties: {
+              name: { type: :string },
+              email: { type: :string }
+            },
+            required: %w[name email]
+          }
+        }
+      }
+      response(201, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        let(:user) { { name: 'foo', email: 'bar' } }
+
         run_test!
       end
     end
