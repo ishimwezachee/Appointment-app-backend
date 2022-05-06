7bc7965b0a7915dab4bda871f95de2df6b000ece
ishimwezachee <zacheeishimwe@gmail.com>
2022-05-06 09:04:03 +0200

    add cardiologists seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 User.create(name: 'Roland', email: 'roland@gamil.com', password: '123456' )
 
-Item.create(name: 'Gints', title: 'Speciality', description: 'Really a good dr.', image: 'https://avatars.githubusercontent.com/u/43877689?v=4', price: 100, location: 'LA')
-
-Item.create(name: 'Aku', title: 'Doctor', description: 'Really a good dr. to be', image: 'https://avatars.githubusercontent.com/u/43877689?v=4', price: 200)
-
+Item.create(name: 'Gints', title: 'Dentist', description: 'Really a good dr.', image: 'https://avatars.githubusercontent.com/u/43877689?v=4')
+Item.create(name: 'Aku', title: 'Hematologists', description: 'Really a good dr. to be', image: 'https://avatars.githubusercontent.com/u/43877689?v=4')
+Item.create(name: 'Mucyo', title: 'Cardiologists', description: 'Really a good dr. to be', image: 'https://avatars.githubusercontent.com/u/43877689?v=4', price: 200)
 Reservation.create(start_time: '2022-05-19', end_time: '2022-05-20', visit_type: 'reservation1', user_id: 1, item_id: 1)
 Reservation.create(start_time: '2022-05-25', end_time: '22022-06-19', visit_type: 'reservation2', user_id: 2, item_id: 2)
